c6dd8287e93765fe4364f82f44a27f1895630504
Marlon Lopes <marlon.lopes@hp.com>
2019-06-08 13:54:41 -0300

    phew

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,25 @@ set :deploy_via, :remote_cache                        # fetches from local git r
 set :stages, ["dev"]
 set :default_stage, "dev"
 
-role :web, "54.186.15.76"                          # Your HTTP server, Apache/etc
-role :app, "54.186.15.76"                          # This may be the same as your `Web` server
+role :web, "34.211.82.57"                          # Your HTTP server, Apache/etc
+role :app, "34.211.82.57"                          # This may be the same as your `Web` server
 
-set :deploy_to, "app/#{application}"
+set :deploy_to, "/app/#{application}"
+# set :current_path, "/app/#{application}/current"
+# set :latest_release, "latest_release"
+
+# print fetch(:current_pat)
 set :scm, :git
 set :user, "ubuntu"
 set :keep_releases, 3
-set :use_sudo, false
+set :use_sudo, true
 
-set :ssh_options, {:forward_agent => true, :verify_host_key => :never, keys: ['~/.ssh/marlon.pem']}
+set :ssh_options, {:forward_agent => true, :verify_host_key => :never, keys: ['~/.ssh/marlon.pem', '~/.ssh/id_rsa']}
 set :default_run_options, {:pty => true}
+
+before :deploy, :ensure_user
+
+task :ensure_user do
+    run "sudo chown ubuntu:ubuntu /app -R"
+end
 
